613093fe11bf628e43e9d26f695d6f2c651734a1
Nico Reher <nreher@student.42berlin.de>
2023-01-04 15:24:40 +0100

    last fixes

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: nreher <nreher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 13:45:59 by nreher            #+#    #+#             */
-/*   Updated: 2022/12/13 13:03:17 by nreher           ###   ########.fr       */
+/*   Updated: 2023/01/04 15:08:08 by nreher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	}
 	temp = ft_lstlast (*lst);
 	temp -> next = new;
-	new -> next = NULL;
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nreher <nreher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 18:17:24 by nreher            #+#    #+#             */
-/*   Updated: 2023/01/03 17:37:19 by nreher           ###   ########.fr       */
+/*   Updated: 2023/01/04 15:22:38 by nreher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	rbck = backremover((char *)s1, (char *)set);
 	rfrnt = frontremover((char *)s1, (char *)set);
 	if (rbck == c)
-		out = ft_substr(s1, rbck, 1);
+		out = ft_substr(s1, rbck + 1, 1);
 	else
 		out = ft_substr(s1, rfrnt, ft_strlen((char *)s1) - rbck - rfrnt);
 	if (out == NULL)
